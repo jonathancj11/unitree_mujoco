582ee4c8cdd146eb223cd864e2c10f4d112afda0
jonathancj <otancj@gmail.com>
2025-03-27 23:21:23 -0300

    ruta binario resultante

--- unitree_mujoco_ros/launch/b2_sim.launch.py
+++ unitree_mujoco_ros/launch/b2_sim.launch.py
@@ -3,12 +3,9 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-    # Aquí asumimos que el binario se llama "simulate_b2" 
-    # o algo así, y está en /opt/unitree_mujoco/simulate/build
-    # ajusta la ruta según tu binario real
     return LaunchDescription([
         ExecuteProcess(
-            cmd=['/opt/unitree_mujoco/simulate/build/simulate_b2'],  # ajusta el nombre real
+            cmd=['/opt/unitree_mujoco/simulate/build/unitree_mujoco']
             output='screen'
         )
     ])
